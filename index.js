a80a7d74c755c537e2a0dcf3354e86dec326cd89
skleenn <113645968+skleenn@users.noreply.github.com>
2023-07-24 16:10:43 -0700

    IMS TRUGLIN

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const commands = [];
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, "commmands");
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+//const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles)
 {
@@ -24,7 +24,7 @@ for (const file of commandFiles)
     const command = require(filePath);
 
     client.commands.set(command.data.name, command);
-    commands.push(command)
+    commands.push(command.data.toJSON())
 }
 
 client.player = new Player(client, {
