005a6d5cef793e432010213b4b58f99cc65af9cb
skleenn <113645968+skleenn@users.noreply.github.com>
2023-07-24 15:19:23 -0700

    chagning

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new Client({
 const commands = [];
 client.commands = new Collection();
 
-const commandsPath = path.join(_dirname, "commmands");
+const commandsPath = path.join(__dirname, "commmands");
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles)
